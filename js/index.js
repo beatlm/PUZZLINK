d7b73fc614a1a2314a54ec54118a5f3d137d8045
Bea <beatlm@gmail.com>
2013-10-16 21:25:16 +0200

    Busqueda puzzles+piezas

--- js/index.js
+++ js/index.js
@@ -41,8 +41,13 @@ function mostrar(id){
 		srchPcs(1);//Buscamos la primera pieza
 	break;
 	case (2):
+	
 		$("#cPuzzlinkers").show();
 		  $("#menu2").addClass( "selected" );
+		  cargaFb(document);
+		//comprobarLogged();
+		//  fbAsyncInit();//comprobamos logueo fb
+		 
 	break;
 	case(3):
 		$("#contenidoA").show("slow");
@@ -271,7 +276,7 @@ function aleatoria(){
 								
 	 
  }
- 
+ /*
  function logar(){
 	// alert($("#login_input_username").val());
 	 //alert($("#login_input_password").val());
@@ -330,4 +335,145 @@ function deslogar(){
 	//var result = JSON.parse(r); 
 	 
 	 
- }
+ }*/
+ 
+ <!-- FUNCIONES FACEBOOK -->
+ function  fbAsyncInit ()  {
+	
+    FB.init({
+      appId      : '442776155842772', // App ID
+      channelUrl : '//WWW.puzzlink.hol.es/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+   
+   
+     // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+  // for any authentication related change, such as login, logout or session refresh. This means that
+  // whenever someone who was previously logged out tries to log in again, the correct case below 
+  // will be handled. 
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+	
+    // Here we specify what we do with the response anytime this event occurs. 
+    if (response.status === 'connected') {
+      // The response object is returned with a status field that lets the app know the current
+      // login status of the person. In this case, we're handling the situation where they 
+      // have logged in to the app.
+      cargaPuzzles(response.authResponse.userID);
+    } else if (response.status === 'not_authorized') {
+      // In this case, the person is logged into Facebook, but not into the app, so we call
+      // FB.login() to prompt them to do so. 
+      // In real-life usage, you wouldn't want to immediately prompt someone to login 
+      // like this, for two reasons:
+      // (1) JavaScript created popup windows are blocked by most browsers unless they 
+      // result from direct interaction from people using the app (such as a mouse click)
+      // (2) it is a bad experience to be continually prompted to login upon page load.
+      FB.login();
+	   $("#introLog").text("Inicia sesión con tu cuenta de Facebook para poder crear puzzles!!");
+    } else {
+      // In this case, the person is not logged into Facebook, so we call the login() 
+      // function to prompt them to do so. Note that at this stage there is no indication
+      // of whether they are logged into the app. If they aren't then they'll see the Login
+      // dialog right after they log in to Facebook. 
+      // The same caveats as above apply to the FB.login() call here.
+	   $("#introLog").text("Inicia sesión con tu cuenta de Facebook para poder crear puzzles!!");
+      FB.login();
+    }
+  });
+  
+
+  };
+
+  // Load the SDK asynchronously
+  function cargaFb(d){
+	 
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/es_ES/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   };
+   
+   
+   // Here we run a very simple test of the Graph API after login is successful. 
+  // This testAPI() function is only called in those cases. 
+  function cargaPuzzles(id) {
+    console.log('Bienvenido!  Obteniendo información.... ');
+    FB.api('/me', function(response) {
+      console.log('Bienvenid@, ' + response.name + '.'+id);
+	 
+	 
+	   $("#introLog").text("Aqui estan tus puzzles "+response.name);
+	   
+	   //Cargamos el nombre de los puzzles asociados al usuario
+	   
+	   var r=	$.ajax({    data:{"id":id},      
+	                url:'php/getPuzzles.php', 
+					type:"POST", async:false
+					
+						        }).responseText;
+								
+	var result = JSON.parse(r);
+	 //Limpiamos las puzzles anteriores
+		 $('#piezas').empty();
+	 for(var i=0;i<result.length;i++){
+		console.log( result[i].nombre);
+		
+		 
+		  	var texto="<div  class='introTxt' onclick='srchPzz("+result[i].id+")'> <p >"+result[i].nombre+"</p>";
+	 		$('#puzzles').append(texto);
+		  
+	 }
+	 
+	   
+	   
+	   
+    });
+  }
+  
+  //Funcion para busqueda de puzzles
+// num-> el numero del puzzle a buscar
+
+function srchPzz(num){	
+	
+	alert(num);
+		 
+	var r=$.ajax({
+	                data:{"id":num},
+	                url:   'php/getPiezasPuzzle.php', 
+					type:"POST",
+					async:false
+					
+						        }).responseText;
+							 
+		var result = JSON.parse(r);
+		alert(result);
+		$("#puzzles").hide();
+			$("#cPiezas").show();
+		
+		$("#txtIntro").text("Hay varias piezas en este puzzle.");
+	  	$("#variasPiezas").show();
+	  	$("#pieza").hide();
+	  	$("#contentPieza").hide();
+		$("#btnLeft").hide();
+		$("#btnRight").hide();
+		 //Limpiamos las piezas anteriores
+		 $('#piezas').empty();
+		  for(var i=0;i<result.length;i++){
+			  alert(result[i].texto);
+		  	var texto="<div  class='introTxt' onclick='srchPcs("+result[i].id+")'> <p >"+result[i].id+"- "+result[i].texto +"</p>";
+	 		$('#piezas').append(texto);
+		  }
+		  
+		  
+	/* for(var i=0;i<result.length;i++){
+		console.log( result[i].nombre);
+	 }*/
+	
+															
+}
+  
+ 
+  
---
+++ php/getPuzzles.php
@@ -0,0 +1,32 @@
+<?php
+define("DB_HOST", "mysql.hostinger.es");
+define("DB_USER", "u185194903_user");//cambiar por el nombre de usuario definido en la configuracion de la BD.
+define("DB_PASSWORD", "puzzBA13");//Modificar por el password elegido
+define("DB_DATABASE", "u185194903_puzzlink");//Nombre de la base de datos reemplazar si se utilizo otro diferente al mencionado en el tutorial.
+ 
+
+$id = $_POST['id'];
+
+$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+        // selecting database
+        mysql_select_db(DB_DATABASE);
+        
+$result=mysql_query("SELECT  p.nombre,p.id FROM puzzles as p WHERE p.userID = '$id'" );
+// $result = mysql_query("INSERT INTO usuarios(username,passw,email) VALUES('$user', '$passw','$email')");
+
+	/*como el usuario debe ser unico cuenta el numero de ocurrencias con esos datos*/
+
+//echo $result;
+
+
+
+$rows = array();
+while($r = mysql_fetch_assoc($result)) {
+    $rows[] = $r;
+}
+echo json_encode($rows);
+
+
+
+ 
+?>
